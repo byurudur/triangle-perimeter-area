e99cfef08aeebe4dd3355a0ec0508ffb6fac4e43
Batu Yürüdür <batuhanyurudur@gmail.com>
2024-02-20 10:10:26 +0300

    2 Modified.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+/**
+ * @author Ahmet Batuhan YÜRÜDÜR
+ * @since 2024
+ */
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+        double  longRim, shortRim, thirdRim, area, perimeter, helper;
+
+        // Collecting inputs;
+        System.out.println("Please enter the first rim: ");
+        shortRim = input.nextDouble();
+        System.out.println("Please enter the second rim: ");
+        longRim = input.nextDouble();
+        System.out.println("Please enter the third rim: ");
+        thirdRim = input.nextDouble();
+
+        // Clearing the mathematical errors;
+        while (true){
+            if (shortRim <= 0 || longRim <= 0 || thirdRim <= 0){
+                System.out.println("Mathematical error, rims can not be negative.");
+                break;
+            }else {
+                // Equations of area and perimeter;
+                perimeter = (longRim + shortRim + thirdRim);
+                System.out.println("Perimeter: " + perimeter);
+                helper = perimeter/2;
+                area = Math.sqrt(helper * (helper-shortRim) * (helper - longRim) * (helper * thirdRim));
+                System.out.println("Area: " + area);
+                break;
+            }
+        }
     }
 }
